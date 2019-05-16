fc5be1362ae9761ff8e7b486968658f43fc12e70
veropra <prainoveronica@gmail.com>
2019-05-16 16:25:46 +0200

    Milestrone 1.2 work in progress...

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ var new_message = $(".write-chat-container.template").clone();
 new_message.children(".write-chat").addclass(".mine").text("<p>acquaman</p>");
 
 $(".real-messages-container").append(new_message);
+
+$(document).ready(function(){
+  $(".right-footer-icon").click(function(){
+    var write-chat = $(".write-msg").val();
+    var new_message = $(".real-messages-container .write-chat .mine").clone();
+  });
+
+});
