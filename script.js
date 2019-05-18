fea9ea870f8df1e6470eb669eec762102c3a9dd9
veropra <prainoveronica@gmail.com>
2019-05-18 17:38:00 +0200

    Milestrone 2 work in progress...

--- script.js
+++ script.js
@@ -1,43 +1,36 @@
 /*MILESTRONE-1*/
 
+/*Clono il template*/
 $(document).ready(function(){
   $(".right-footer-icon").click(function(){
     var write_chat = $(".message-bar").val();
-    new_message = $(".chat-message-template").clone();
+    new_message = $(".template .chat-message-template").clone();
     new_message.addClass("mine").children(".write-chat").text(write_chat);
     $(".chat-container").append(new_message);
     $(".message-bar").val("");
   });
 });
 
+/*Funzione per far scoparire il microfono quando scrivo nella barra dei messaggi
+e far apparire lo share-square e viceversa*/
+  $(".message-bar").click(function(){
+    sendMessage();
+  });
 
-$(".message-bar").keyup(function() {
-  $(".send i").removeClass("fa-microphone");
-  $(".send i").addClass("fa-arrow-alt-circle-right");
-});
+  $('.write-msg input').focus(function(){
 
+    $(".send i").toggleClass("fa-microphone fa-share-square");
+  }).blur(function(){
+    $(".send i").toggleClass("fa-microphone fa-share-square");
+  });
 
-function time() {
-  var ore = new Date().getHours();
-  var minuti = new Date().getMinutes();
-
-  if (ore < 10) {
-  ore = "0" + ore;
-  }
-
-  if (minuti < 10) {
-  minuti = "0" + minuti;
-  }
-  $(".time").html(ore + ":" + minuti);
-}
 
 /*MILESTRONE-2*/
-
+/*Funzione per ricevere il messaggio di risposta ok*/
 function ok() {
-  var ok_message = $(".chat-message-template").clone();
-  ok_message.removeClass("template");
-  ok_message.children(".write_chat").children(".message").html("Ok!");
-  ok_message.children(".write_chat;").removeClass("utente");
+  var ok_message = $(".chat-container .template").clone();
+  ok_message.children(".ok").children(".ok").html("Ok!");
+  ok_message.children(".ok").removeClass("ok");
   $(".chat-container").append(ok_message);
 }
 
